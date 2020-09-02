7248a5e5426f4c67b10062b2e6e06934dc885c16
Amir Kalali <a.kalali@dkfz-heidelberg.de>
2020-09-02 13:50:23 +0200

    Add third party - DCMQI - as external project

---
+++ SuperBuild.cmake
@@ -0,0 +1,24 @@
+#----------
+# External project settings
+#----------
+
+# Set EP_BASE PROPERTY for a more compact ep-directory structure
+set(EP_BASE "${CMAKE_BINARY_DIR}/ep")
+set_property(DIRECTORY PROPERTY EP_BASE ${EP_BASE})
+
+# Add external projects
+include(ExternalProject)
+include(CMakeExternals/DCMQI.cmake)
+
+# Add custom external project to process inner build
+ExternalProject_Add(ConfigureBuild
+  SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}
+  DOWNLOAD_COMMAND ""
+  UPDATE_COMMAND ""
+  INSTALL_COMMAND ""
+  CMAKE_CACHE_ARGS
+    -DUSE_SUPERBUILD:BOOL=OFF
+    -DCONAN_CMAKE:BOOL=${CONAN_CMAKE}
+    -DCONAN_CMD:FILEPATH=${CONAN_CMD}
+  BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/Hello-World
+)
