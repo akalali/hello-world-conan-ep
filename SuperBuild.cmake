2dc7071e6866de4ce55be5f2475df95ccea27c1d
Amir Kalali <a.kalali@dkfz-heidelberg.de>
2021-03-25 11:04:45 +0100

    Move Conan build to superbuild
    Superbuild should be responsible for generating all third-party dependencies.

--- SuperBuild.cmake
+++ SuperBuild.cmake
@@ -1,4 +1,30 @@
 #----------
+# Conan projects
+#----------
+option (USE_CONAN "Whether or not Conan should be used" ON)
+if (USE_CONAN)
+  set(CONAN_CMAKE FALSE CACHE BOOL
+    "Enable to download third party dependencies with Conan via CMake. Disable to install them with Conan separately.")
+  set(CONAN_CMD "" CACHE FILEPATH
+    "File-path to the Conan executable")
+
+  if(CONAN_CMAKE)
+    # Run Conan from CMake to install third party libraries
+    message(STATUS "\nInstall libraries using Conan")
+    include(Conan/download_conan.cmake)
+    include(Conan/dcmtk_conan_options.cmake)
+    include(Conan/itk_conan_options.cmake)
+    include(Conan/conan_run.cmake)
+  else()
+    message(FATAL_ERROR "Requirements not installed\n"
+                        "Please enable CONAN_CMAKE and set the path to the Conan executable with CONAN_CMD.")
+  endif()
+
+  set(CMAKE_MODULE_PATH ${CMAKE_BINARY_DIR} ${CMAKE_MODULE_PATH})
+  set(CMAKE_PREFIX_PATH ${CMAKE_BINARY_DIR} ${CMAKE_PREFIX_PATH})
+endif()
+
+#----------
 # External project settings
 #----------
 
